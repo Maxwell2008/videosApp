44ceb3c94693e54e31b189530b8b12a1fb86e9c1
maxwell2008 <emersonmaxwell@yahoo.com.br>
2021-10-04 19:33:39 -0300

    adicionado TOAST mensagem de confirmacao

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { Animation } from '@capacitor/status-bar';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +11,39 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
+
+  async exibirAlertFavorito() {
+    const alert = await this.alertController.create({
+      header: 'Atenção!',
+      message: 'Deseja realmente favoritar o Filme?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim Favoritar!',
+          handler: () => {
+            this.apresentarToast();
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+  async apresentarToast() {
+    const toast = await this.toastController.create({
+      message: 'FILME ADICIONADO COM SUCESSO!',
+      duration: 2000,
+      color:'success',
+
+    });
+    toast.present();
+  }
+
 
 }
